7e6d1b860aa1844b9f398a3429a941260e8850f5
Justin Krometis <jkrometis@vt.edu>
2021-09-10 16:40:15 -0400

    experimenting with menus

--- docs/conf.py
+++ docs/conf.py
@@ -36,7 +36,12 @@ extensions = ['myst_parser']
 templates_path = ['_templates']
 
 # The suffix of source filenames.
-source_suffix = '.rst'
+#source_suffix = '.rst'
+source_suffix = {
+    '.rst': 'restructuredtext',
+    '.txt': 'restructuredtext',
+    '.md': 'markdown',
+}
 
 # The encoding of source files.
 #source_encoding = 'utf-8-sig'
